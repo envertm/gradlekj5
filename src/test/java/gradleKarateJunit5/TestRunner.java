67291f09a87b067fc1b41167a736fb95c970e9f8
Enver <=>
2022-07-03 19:15:47 -0500

    upload

--- src/test/java/gradleKarateJunit5/TestRunner.java
+++ src/test/java/gradleKarateJunit5/TestRunner.java
@@ -29,39 +29,39 @@ import net.masterthought.cucumber.ReportBuilder;
 
 public class TestRunner {
 	
-//	   @Test
-//	    public void testParallel() {
-//	        Results results = Runner.path("classpath:gradleKarateJunit5")
-//	                .outputCucumberJson(true)
-//	           //     .karateEnv("demo")
-//	                .parallel(5);
-//	        generateReport(results.getReportDir());
-//	        assertEquals(results.getErrorMessages(), results.getFailCount() == 0);
-//	    }
-//
-//	    public static void generateReport(String karateOutputPath) {
-//	        Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[] {"json"}, true);
-//	        List<String> jsonPaths = new ArrayList<>(jsonFiles.size());
-//	        jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
-//	        Configuration config = new Configuration(new File("target"), "demo");
-//	        ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
-//	        reportBuilder.generateReports();
-//	    }
-	
-	@Test
-	void testParallel() {
-		// System.setProperty("karate.env", "privada");
-		Results results = Runner.path("classpath:gradleKarateJunit5").outputCucumberJson(true).parallel(5);
-		generateReport(results.getReportDir());
-		assertEquals(0, results.getFailCount(), results.getErrorMessages());
-	}
+	   @Test
+	    public void testParallel() {
+	        Results results = Runner.path("classpath:gradleKarateJunit5")
+	                .outputCucumberJson(true)
+	           //     .karateEnv("demo")
+	                .parallel(5);
+	        generateReport(results.getReportDir());
+	        assertEquals(results.getErrorMessages(), results.getFailCount() == 0);
+	    }
 
-	public static void generateReport(String karateOutputPath) {
-		Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[] { "json" }, true);
-		List<String> jsonPaths = new ArrayList<>(jsonFiles.size());
-		jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
-		Configuration config = new Configuration(new File("target"), "gradleKarateJunit5");
-		ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
-		reportBuilder.generateReports();
-	}
+	    public static void generateReport(String karateOutputPath) {
+	        Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[] {"json"}, true);
+	        List<String> jsonPaths = new ArrayList<>(jsonFiles.size());
+	        jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
+	        Configuration config = new Configuration(new File("target"), "demo");
+	        ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
+	        reportBuilder.generateReports();
+	    }
+	
+//	@Test
+//	void testParallel() {
+//		// System.setProperty("karate.env", "privada");
+//		Results results = Runner.path("classpath:gradleKarateJunit5").outputCucumberJson(true).parallel(5);
+//		generateReport(results.getReportDir());
+//		assertEquals(0, results.getFailCount(), results.getErrorMessages());
+//	}
+//
+//	public static void generateReport(String karateOutputPath) {
+//		Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[] { "json" }, true);
+//		List<String> jsonPaths = new ArrayList<>(jsonFiles.size());
+//		jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
+//		Configuration config = new Configuration(new File("target"), "gradleKarateJunit5");
+//		ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
+//		reportBuilder.generateReports();
+//	}
 }
